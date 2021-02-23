b04a2a740ec739d901dfec5e23f75be7441ea67c
jamesElls <jamellsworth@gmail.com>
2021-02-23 09:41:50 -0700

    Create functionality

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+Page.destroy_all
+
+10.times do
+  Page.create(
+    title: Faker::Hacker.abbreviation,
+    description: Faker::Lorem.body,
+  )
+end
+
+puts "Seeded you have #{Page.all.size} pages
